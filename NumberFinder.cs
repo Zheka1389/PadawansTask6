5f23a7f909114c1bcbd5833c2c8c6d28f57ca35b
Prystauka Yauheni <34281826+Zheka1389@users.noreply.github.com>
2019-02-14 21:41:11 +0300

    1

--- NumberFinder.cs
+++ NumberFinder.cs
@@ -6,8 +6,49 @@ namespace PadawansTask6
     {
         public static int? NextBiggerThan(int number)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (number <= 0)
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                if (number == int.MaxValue)
+                {
+                    return null;
+                }
+                char[] l = number.ToString().ToCharArray();
+                Array.Sort(l);
+                char[] lr = new char[l.Length];
+                lr = (char[])l.Clone();
+                Array.Reverse(lr);
+                string g = new string(lr);
+                int g1 = Convert.ToInt32(g);
+                checked
+                {
+                    for (int i = number + 1; i <= g1; i++)
+                    {
+                        char[] ch = i.ToString().ToCharArray();
+                        Array.Sort(ch);
+                        if (CharArrayCompare(l, ch))
+                        {
+                            return i;
+                        }
+                    }
+                    return null;
+                }
+            }
+        }
+        private static bool CharArrayCompare(char[] charLeft, char[] charRight)
+        {
+            if (charLeft.Length != charRight.Length)
+                return false;
+            var length = charLeft.Length;
+            for (int i = 0; i < length; i++)
+            {
+                if (charLeft[i] != charRight[i])
+                    return false;
+            }
+            return true;
         }
     }
 }
